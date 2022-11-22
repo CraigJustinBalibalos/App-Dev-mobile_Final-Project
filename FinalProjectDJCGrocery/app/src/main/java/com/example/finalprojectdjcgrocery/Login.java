25b0934edf3d333946ddc474fa0511a9db926727
Justom <cjustin1@outlook.com>
2022-11-22 18:10:36 -0500

    Connected Firebase and did MainActivity back end

--- FinalProjectDJCGrocery/app/src/main/java/com/example/finalprojectdjcgrocery/Login.java
+++ FinalProjectDJCGrocery/app/src/main/java/com/example/finalprojectdjcgrocery/Login.java
@@ -3,12 +3,30 @@ package com.example.finalprojectdjcgrocery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    EditText username, password;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.inputUsername);
+        password = findViewById(R.id.inputPassword);
+
+        loginBtn = findViewById(R.id.loginBtn2);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- FinalProjectDJCGrocery/app/src/main/java/com/example/finalprojectdjcgrocery/MainActivity.java
+++ FinalProjectDJCGrocery/app/src/main/java/com/example/finalprojectdjcgrocery/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.finalprojectdjcgrocery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        registerBtn = findViewById(R.id.registerBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent i = new Intent(getApplicationContext(), Register.class);
+                //startActivity(i);
+            }
+        });
     }
 }
