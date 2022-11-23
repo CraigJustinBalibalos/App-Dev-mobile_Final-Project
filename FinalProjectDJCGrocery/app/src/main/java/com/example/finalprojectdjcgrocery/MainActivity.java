0ca4584b96b617791787c49e3e1329941fa865af
Justom <cjustin1@outlook.com>
2022-11-22 21:44:18 -0500

    Updated register back end but does not save in firebase

--- FinalProjectDJCGrocery/app/src/main/java/com/example/finalprojectdjcgrocery/MainActivity.java
+++ FinalProjectDJCGrocery/app/src/main/java/com/example/finalprojectdjcgrocery/MainActivity.java
@@ -30,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent i = new Intent(getApplicationContext(), Register.class);
-                //startActivity(i);
+                Intent i = new Intent(getApplicationContext(), Register.class);
+                startActivity(i);
             }
         });
     }
